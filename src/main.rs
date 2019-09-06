c4e5235b7fac64b415d1d10aa41dfdbfff14f05e
Heinz N. Gies <heinz@licenser.net>
2019-09-06 20:28:42 +0200

    Recv loop

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,16 @@
 use mio::net::UdpSocket;
 use mio::{Events, Poll, PollOpt, Ready, Token};
-use std::time::Duration;
 use std::thread;
-
+use std::time::Duration;
 
 const IN: Token = Token(0);
-
 
 fn main() {
     let poll = Poll::new().unwrap();
 
     let socket = UdpSocket::bind(&format!("{}:{}", "0.0.0.0", 9000).parse().unwrap()).unwrap();
-    poll.register(&socket, IN, Ready::readable(), PollOpt::edge()).unwrap();
+    poll.register(&socket, IN, Ready::readable(), PollOpt::edge())
+        .unwrap();
     //poll.register(&socket, IN, Ready::readable(), PollOpt::level()).unwrap();
 
     let mut events = Events::with_capacity(1024);
@@ -21,17 +20,24 @@ fn main() {
         use std::net::UdpSocket;
         let socket = UdpSocket::bind("127.0.0.1:34254").expect("couldn't bind to address");
         loop {
-          socket.send_to(b"Argle bargle, foofaraw, hey diddy ho diddy no one knows", ("127.0.0.1", 9000));
-       }
+            socket.send_to(
+                b"Argle bargle, foofaraw, hey diddy ho diddy no one knows",
+                ("127.0.0.1", 9000),
+            );
+        }
     });
 
     loop {
-        poll.poll(&mut events, Some(Duration::from_millis(100))).unwrap();
+        poll.poll(&mut events, Some(Duration::from_millis(100)))
+            .unwrap();
         for event in events.iter() {
             match event.token() {
                 IN => {
-                    let n = socket.recv(&mut buf).unwrap();
-                    print!(".");
+                    let mut n = socket.recv(&mut buf).unwrap();
+                    while n != 0 {
+                        print!(".");
+                        n = socket.recv(&mut buf).unwrap();
+                    }
                 }
                 _ => unreachable!(),
             }
